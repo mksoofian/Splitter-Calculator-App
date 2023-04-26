87a5d10342b1c5a52146a6e24b9aca9f11b72c62
Mike S <95092046+mksoofian@users.noreply.github.com>
2023-04-26 12:37:25 -0400

    Updated Hover State Effects and Layout

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const tipSelectors = document.querySelector(".tip_selectors");
 const tipInput = document.querySelector("#tip_input");
 const resetBtn = document.querySelector("#reset_btn");
 const errorMsg = document.querySelector(".error_msg");
+const billInputError = document.querySelector(".billInputError");
+const peopleInputError = document.querySelector(".peopleInputError");
 //button selectors
 const fivePerc = document.querySelector("#five");
 const tenPerc = document.querySelector("#ten");
@@ -21,17 +23,17 @@ let partySize;
 
 /////Functions
 
-//Updates text to be right aligned in Tip Input when active
-tipInput.addEventListener("change", () => {
-  if (tipInput.value !== "") {
-    tipInput.style.textAlign = "right";
-  } else {
-    tipInput.style.textAlign = "center";
-  }
-});
-
 billRecieved.addEventListener("change", () => {
   bill = Number(billRecieved.value);
+  //Error Message if billRecieved.value == 0
+  if (billRecieved.value <= 0) {
+    billRecieved.value = "0";
+    billInputError.textContent = "Can't be Zero";
+    billRecieved.style.border = "2px solid rgba(219, 101, 4, 0.877)";
+  } else {
+    billInputError.textContent = "";
+    billRecieved.style.border = "none";
+  }
 });
 
 numOfPeople.addEventListener("change", () => {
@@ -39,24 +41,19 @@ numOfPeople.addEventListener("change", () => {
   //Error Message if NumofPeople == 0
   if (partySize <= 0) {
     numOfPeople.value = "0";
-    errorMsg.textContent = "Can't be Zero";
+    peopleInputError.textContent = "Can't be Zero";
     numOfPeople.style.border = "2px solid rgba(219, 101, 4, 0.877)";
   } else {
-    errorMsg.textContent = "";
+    peopleInputError.textContent = "";
     numOfPeople.style.border = "none";
   }
 });
-
+//Updates text-align right in Tip Input when active
 tipInput.addEventListener("change", () => {
-  tip = Number(tipInput.value / 100);
-  for (y = 0; y < percBtnsArr.length; y++) {
-    if (Number(percBtnsArr[y].lastElementChild.textContent) == tipInput.value) {
-      btnColorToggleOn(percBtnsArr[y]);
-    } else if (
-      Number(percBtnsArr[y].lastElementChild.textContent) !== tipInput.value
-    ) {
-      btnColorToggleOff(percBtnsArr[y]);
-    }
+  if (tipInput.value !== "") {
+    tipInput.style.textAlign = "right";
+  } else {
+    tipInput.style.textAlign = "center";
   }
 });
 
@@ -106,7 +103,26 @@ function updateTipPerPerson(value) {
 function updateTotalPerPerson(value) {
   document.querySelector(".result2").textContent = value;
 }
-function updateResults() {
+function updateAll() {
+  //Activate reset button
+  if (billRecieved > 0 || tipInput > 0 || numOfPeople > 0) {
+    resetBtn.style.color = "var(--verydarkCyan)";
+    resetBtn.style.backgroundColor = "var(--strongCyan)";
+    resetBtn.disabled = false;
+    resetBtn.onmouseover = function () {
+      this.style.backgroundColor = "var(--activeCyan)";
+    };
+    resetBtn.onmouseout = function () {
+      this.style.backgroundColor = "var(--strongCyan)";
+    };
+  }
+  //De-activate reset button
+  if (bill <= 0 || tip <= 0 || partySize <= 0) {
+    resetBtn.style.color = "var(--verydarkCyan)";
+    resetBtn.style.backgroundColor = "hsla(182, 43%, 30%, 0.644)";
+    resetBtn.disabled = true;
+  }
+  //calculate results
   if (bill > 0 && tip > 0 && partySize > 0) {
     let tipTotal = bill * tip;
     let tipAmount = tipTotal / partySize;
@@ -115,7 +131,7 @@ function updateResults() {
     updateTotalPerPerson(totalPerPerson.toFixed(2));
   }
   //Reset Function
-  resetBtn.addEventListener("click", () => {
+  const reset = resetBtn.addEventListener("click", () => {
     billRecieved.value = "";
     bill = 0;
     tip = "";
@@ -125,14 +141,16 @@ function updateResults() {
     tipAmount = 0;
     totalPerPerson = 0;
     tipInput.style.textAlign = "center";
+    // resetBtn.style.color =
     updateTipPerPerson(tipAmount.toFixed(2));
     updateTotalPerPerson(totalPerPerson.toFixed(2));
   });
+  reset();
 }
 //Listeners
 document.addEventListener("change", () => {
-  updateResults();
+  updateAll();
 });
 document.addEventListener("click", () => {
-  updateResults();
+  updateAll();
 });
